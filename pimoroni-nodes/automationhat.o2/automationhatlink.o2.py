e4696f75ea41b58402477c2e912ba938236679c1
Ronny Van den Broeck <ronny.vandenbroeck@telenet.be>
2017-12-27 01:57:46 +0100

    adding all files

---
+++ pimoroni-nodes/automationhat.o2/automationhatlink.o2.py
@@ -0,0 +1,161 @@
+#!/usr/bin/env python
+
+import sys
+import time
+import atexit
+from threading import Thread, Event
+from Queue import Queue, Empty
+
+""" Check if we can find the GPIO library and necessary classes """
+try:
+    import RPi.GPIO as GPIO
+except ImportError:
+    exit("This library requires the RPi.GPIO module\nInstall with: sudo pip install RPi.GPIO")
+
+
+""" Define the RPI pin class """
+class Pin(object):
+    type = 'Pin'
+
+    def __init__(self, pin):
+        self.pin = pin
+        self._last_value = None
+
+    def __call__(self):
+        return filter(lambda x: x[0] != '_', dir(self))
+
+    def read(self):
+        return GPIO.input(self.pin)
+
+    def has_changed(self):
+        value = self.read()
+
+        if self._last_value is None:
+            self._last_value = value
+
+        if value is not self._last_value:
+            self._last_value = value
+            return True
+
+        return False
+
+    def is_on(self):
+        return self.read() == 1
+
+    def is_off(self):
+        return self.read() == 0
+
+    
+""" Define output class to drive the output pin """
+class Output(Pin):
+    type = 'Digital Output'
+
+    def __init__(self, pin):
+        Pin.__init__(self, pin)
+        GPIO.setup(self.pin, GPIO.OUT, initial=0)
+
+    def write(self, value):
+        GPIO.output(self.pin, value)
+
+    def on(self):
+        self.write(1)
+
+    def off(self):
+        self.write(0)
+
+    def toggle(self):
+        self.write(not self.read())
+
+
+""" Initialize our raspberry pi GPIO subsys """
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+
+def _cleanup():
+    GPIO.cleanup()
+
+atexit.register(_cleanup)
+
+
+""" Define a non-blocking stream reader """
+class NonBlockingStreamReader:
+
+    def __init__(self, stream):
+        '''
+        stream: the stream to read from.
+                Usually a process' stdout or stderr.
+        '''
+
+        self._s = stream
+        self._q = Queue()
+        self._stop_event = Event()
+
+        def _populateQueue(stream, queue, stop_event):
+            '''
+            Collect lines from 'stream' and put them in 'queue'.
+            '''
+            while not stop_event.is_set():
+                line = stream.readline()
+                if line:
+                    queue.put(line)
+
+        self._t = Thread(target = _populateQueue,
+                args = (self._s, self._q, self._stop_event))
+        self._t.daemon = True
+        self._t.start() #start collecting lines from the stream
+
+    def readline(self, timeout = None):
+        try:
+            return self._q.get(block = timeout is not None, timeout = timeout)
+        except Empty:
+            return None
+
+    def stop(self):
+        self._stop_event.set()
+
+
+""" Define a timer to measure milliseconds """
+def millis():
+    return int(round(time.time() * 1000))
+
+""" Define some logging classes """
+def emit(message):
+    sys.stdout.write(message + "\n")
+    sys.stdout.flush()
+
+def info(message):
+    emit("INFO: " + message)
+
+def error(message):
+    emit("ERROR: " + message)
+
+def fatal(message):
+    emit("FATAL: " + message)
+    sys.exit(1)
+
+
+""" and here we go """
+info("Starting up...")
+running = True
+info("Start reading input stream...")
+stdin = NonBlockingStreamReader(sys.stdin)
+
+def handle_command(cmd):
+    if cmd is not None:
+        if type(cmd).__name__ == 'str':
+            info("Incoming string message, value: " + cmd)
+            if cmd.strip() == 'true' or cmd.strip() == '1': 
+                Output(4).on()
+            elif cmd.strip() == 'false' or cmd.strip() == '0':
+                Output(4).on()
+            elif cmd.strip() == 'toggle' or cmd.strip() == '2':
+                Output(4).on()
+            else:
+                error("Incoming message ignored, only accept boolean, string or integer, with values: true,false,toggle,0,1,2")
+
+info("Watching for incoming messages...")
+while running:
+    cmd = stdin.readline(0.1)
+    handle_command(cmd)
+    time.sleep(0.001)
---
+++ pimoroni-nodes/automationhat.o2/rpi-automationhat.o2.js
@@ -0,0 +1,217 @@
+/**
+ * Copyright 2016 Pimoroni Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+module.exports = function(RED) {
+    "use strict";
+
+	var nodename = "Automationhat o2";
+
+	/* 
+	Below functions are related to error handling. 
+	They can be used throughout the javascript code in this node to
+    log various error message of a certain priority.
+	The log level defined in the node-red settings file will determine
+	which of these log messages will be displayed on the console.
+	*/
+
+	function LogFatal(message){
+		RED.log.fatal(nodename + ": " + message);
+	}
+
+	function LogError(message){
+		RED.log.error(nodename + ": " + message);
+	}
+
+	function LogWarning(message){
+		RED.log.error(nodename + ": " + message);
+	}
+	
+	function LogInfo(message){
+		RED.log.info(nodename + ": " + message);
+	}
+
+	function LogDebug(message){
+		RED.log.debug(nodename + ": " + message);
+	}
+
+	function LogTrace(message){
+		RED.log.trace(nodename + ": " + message);
+	}
+
+
+	// If this node get's loaded, it will receive an object called 'RED'	
+	// If the loglevel is set to tracing, we will dump this object to the console
+	if (RED.settings.logging.console.level == "trace"){ 
+		LogTrace("Received the following parameters from the runtime engine: \n\n" + JSON.stringify(RED) + "\n");
+	}
+
+
+	// Executing our python script
+
+	var HAT = (function(){
+
+        var fs = require("fs");
+        var spawn = require("child_process").spawn;
+        var cmd = __dirname+"/automationhatlink.o2";
+        var hat = null;
+        var allowExit = false;
+        var reconnectTimer = null;
+        var disconnectTimeout = null;
+        var users = [];
+
+        if ( !(fs.statSync(cmd).mode & 1) ) {
+            LogError("Python script must be executable (755)");
+	    throw "Error: '" + cmd + "' must be executable (755)";
+        }
+
+        process.env.PYTHONBUFFERED = 1;
+
+        var connect = function() {
+            if( reconnectTimer ) clearTimeout(reconnectTimer);
+
+            reconnectTimer = null;
+            allowExit = false;
+
+            hat = spawn(cmd);
+
+            users.forEach(function(node){
+                node.status({fill:"green",shape:"dot",text:"Connected"});
+            });
+
+            function handleMessage(data){
+                data = data.trim();
+                if (data.length == 0) return;
+
+                if (data.substring(0,5) == "ERROR"){
+                    LogError(data);
+                    return;
+                }
+
+                if (data.substring(0,5) == "FATAL"){
+                    LogFatal(data);
+		    throw "There was a fatal error, can't continue" + data;
+                }
+            }
+
+            hat.stdout.on('data', function(data) {
+
+		LogTrace("Received message from python script: " + data);
+
+                data = data.toString().trim();
+                if (data.length == 0) return;
+
+                var messages = data.split("\n");
+                messages.forEach(function(message){
+                    handleMessage(message);
+                });
+
+            });
+
+            hat.stderr.on('data', function(data) {
+                LogError("Process Error: "+data+" :");
+
+                hat.stdin.write("stop");
+                hat.kill("SIGKILL");
+            });
+
+            hat.on('close', function(code) {
+                LogWarning("Process Exit: "+code+" :");
+
+                hat = null;
+                users.forEach(function(node){
+                    node.status({fill:"red",shape:"circle",text:"Disconnected"});
+                });
+
+                if (!allowExit && !reconnectTimer){
+                    LogInfo("Attempting Reconnect");
+
+                    reconnectTimer = setTimeout(function(){
+                        connect();
+                    },5000);
+                }
+
+            });
+
+        }
+
+        var disconnect = function(){
+            disconnectTimeout = setTimeout(function(){
+                if (hat !== null) {
+                    allowExit = true;
+                    hat.stdin.write("stop\n");
+                    hat.kill("SIGKILL");
+                }
+            },3000);
+            if (reconnectTimer) {
+                clearTimeout(reconnedTimer);
+            }
+
+        }
+
+        return {
+            open: function(node){
+                if (disconnectTimeout) clearTimeout(disconnectTimeout);
+                if (!hat) connect();
+
+                if(!reconnectTimer){
+                    node.status({fill:"green",shape:"dot",text:"Connected"});
+                }
+
+                users.push(node);
+
+                LogInfo("Adding node, count: " + users.length.toString());
+            },
+            close: function(node,done){
+                users.splice(users.indexOf(node),1);
+                
+                LogInfo("Removing node, count: " + users.length.toString());
+
+                if(users.length === 0){
+                    disconnect();
+                }
+            },
+            send: function(msg){
+                if(hat) hat.stdin.write(msg+"\n");
+            }
+        }
+
+
+    })();
+
+    function AutomationhatOut(config) {
+	RED.nodes.createNode(this,config);
+ 
+        var node = this;
+
+        HAT.open(this);
+
+        node.on("input", function(msg) {
+		    LogTrace("Node has incoming message...");
+		    if ( (typeof msg.payload === "boolean") || (typeof msg.payload === "string") || (typeof msg.payload === "number") ){
+                HAT.send(msg.payload);
+                LogTrace("Sending " + typeof msg.payload + " message to python script, value is: " + msg.payload.toString());
+            }
+		
+
+        });
+
+        node.on("close", function(done) {
+            done();
+        });
+    }
+
+    RED.nodes.registerType("rpi-automationhat o2",AutomationhatOut);
+}
